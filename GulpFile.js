d8a8b6ccb809f0a343dd01f2c6fbe495f753ea00
SiebenVanLommel <sieben.vanlommel@gmail.com>
2024-08-18 21:05:29 +0200

    tested and reworked code

--- GulpFile.js
+++ GulpFile.js
@@ -3,6 +3,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
+const sharp = require('gulp-sharp-responsive');
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 const postcss = require('gulp-postcss');
@@ -34,11 +35,33 @@ function scripts() {
     .pipe(browserSync.stream());
 }
 
-// Image task: optimizes images
+// Optimize PNG images using sharp
+function optimizePng() {
+  return gulp.src('Frontend/Src/images/**/*.png')
+    .pipe(sharp({
+      formats: [
+        {
+          format: 'png',
+          width: 1024,
+          pngOptions: {
+            quality: 80,
+            progressive: true,
+            compressionLevel: 9,
+            effort: 10,
+          },
+        },
+      ],
+    }))
+    .pipe(gulp.dest('Frontend/Dist/images/'))
+    .pipe(browserSync.stream());
+}
+
+// Optimize other images with imagemin
 function images() {
-  return gulp.src(files.imgPath)
+  return gulp.src('Frontend/Src/images/**/*.{jpg,jpeg,gif,svg}')
     .pipe(imagemin())
-    .pipe(gulp.dest('Frontend/Dist/images/'));
+    .pipe(gulp.dest('Frontend/Dist/images'))
+    .pipe(browserSync.stream());
 }
 
 // Watch task: watch SCSS and JS files for changes
@@ -51,15 +74,17 @@ function watch() {
   });
   gulp.watch(files.scssPath, styles);
   gulp.watch(files.jsPath, scripts);
-  gulp.watch(files.imgPath, images);
+  gulp.watch('Frontend/Src/images/**/*.png', optimizePng); // Watch PNG images for optimization
+  gulp.watch('Frontend/Src/images/**/*.{jpg,jpeg,gif,svg}', images); // Watch other images
   gulp.watch('./*.html').on('change', browserSync.reload);
 }
 
 // Export the tasks
 exports.styles = styles;
 exports.scripts = scripts;
+exports.optimizePng = optimizePng;
 exports.images = images;
 exports.watch = watch;
 
 // Default task: runs all tasks and starts watch
-exports.default = gulp.series(styles, scripts, images, watch);
+exports.default = gulp.series(styles, scripts, optimizePng, images, watch);
